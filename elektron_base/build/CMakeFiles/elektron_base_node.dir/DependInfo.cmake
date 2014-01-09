160f214b4b421430baecebf1e051f3a7f71bbd7d
robot <robot@elektron2.(none)>
2014-01-09 15:38:07 +0100

    a

---
+++ elektron_base/build/CMakeFiles/elektron_base_node.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/robot/ros/elektron_robot/elektron_base/src/nf/nfv2.c" "/home/robot/ros/elektron_robot/elektron_base/build/CMakeFiles/elektron_base_node.dir/src/nf/nfv2.o"
+  "/home/robot/ros/elektron_robot/elektron_base/src/nfv2_config.c" "/home/robot/ros/elektron_robot/elektron_base/build/CMakeFiles/elektron_base_node.dir/src/nfv2_config.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/robot/ros/elektron_robot/elektron_base/src/elektron_base_node.cpp" "/home/robot/ros/elektron_robot/elektron_base/build/CMakeFiles/elektron_base_node.dir/src/elektron_base_node.o"
+  "/home/robot/ros/elektron_robot/elektron_base/src/serialcomm/serialcomm.cpp" "/home/robot/ros/elektron_robot/elektron_base/build/CMakeFiles/elektron_base_node.dir/src/serialcomm/serialcomm.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ elektron_base/build/CMakeFiles/elektron_base_node.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/elektron_base_node.dir/src/elektron_base_node.o"
+  "CMakeFiles/elektron_base_node.dir/src/serialcomm/serialcomm.o"
+  "CMakeFiles/elektron_base_node.dir/src/nf/nfv2.o"
+  "CMakeFiles/elektron_base_node.dir/src/nfv2_config.o"
+  "../bin/elektron_base_node.pdb"
+  "../bin/elektron_base_node"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/elektron_base_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
